01134db6986c7694ee6d9cf1e3109fa6769757d2
NektoUa <illyabarkov@gmail.com>
2022-01-12 17:09:38 +0200

    add generation

--- templates/generation.php
+++ templates/generation.php
@@ -18,3 +18,79 @@ $resSQL = $mysqli->query($sql);
 </header>
 <?php
 }
+
+function postsIndex($mysqli) {
+    $sql = "SELECT * FROM `articles`";
+    $res = $mysqli->query($sql);
+    if ($res->num_rows > 0) {
+        while ($resArticle = $res->fetch_assoc()) {
+            ?>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title" ><a href="post.php?id_article=<?= $resArticle['id'] ?>"><?= $resArticle['title'] ?></a></h5>
+                    <p class="card-text"><?= mb_substr($resArticle['text'], 0, 158, 'UTF-8') ?></p>
+                </div>
+            </div>
+            <?php
+        }
+    } else {
+               echo "Нет статей";
+    }
+}
+        }
+    }
+}
+
+function postsTopic($mysqli, $id_topic) {
+    $sql = "SELECT * FROM `articles` WHERE `id_topic` = $id_topic";
+    $res = $mysqli -> query($sql);
+    if ($res -> num_rows > 0) {
+        while ($resArticle = $res -> fetch_assoc()) {
+            ?>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title" ><a href="post.php?id_article=<?= $resArticle['id'] ?>"><?= $resArticle['title'] ?></a></h5>
+                    <p class="card-text"><?= mb_substr($resArticle['text'], 0, 158, 'UTF-8') ?></p>
+                </div>
+            </div>
+            <?php
+        }
+    } else {
+        echo "В этом раздели статей нету";
+    }
+}
+    }
+}
+
+function generationPost ($mysqli, $id_article) {
+       $sql = "SELECT * FROM `articles` WHERE `id` = '$id_article'";
+       $res = $mysqli -> query($sql);
+       if ($res -> num_rows === 1) {
+               $resPost = $res -> fetch_assoc()?>
+        <h1><?= $resPost['title'] ?></h1>
+        <p><?= $resPost['text'] ?></p>
+        <p>Дата публикации: <?= substr($resPost['date'], 0, 11) ?></p>
+        <?php
+    }
+}
+
+function generation_comment ($mysqli, $id_article) {
+       $sql = "SELECT * FROM `comments` WHERE `id_article` = $id_article";
+   
+    $resSQL = $mysqli -> query($sql);
+       if ($resSQL -> num_rows > 0) {
+              while ($resComment = $resSQL -> fetch_assoc()) {
+            ?> 
+            <div class="comment">
+                <p><b><?= $resComment['comment']?></b></p>
+                <p>Оставлен: <?= substr($resComment['date'], 0, 11)  ?></p>
+            </div>
+            <hr>
+            <?php
+        }
+    } else {
+               ?>
+            <p>Комментариев нет</p>
+        <?php
+    }
+}
